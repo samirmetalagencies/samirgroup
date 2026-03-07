961ad241127d78a8ed297ef8227efbf3acef1a6c
samirmetalagencies <samirmetalagencies@gmail.com>
2026-03-07 23:39:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById("catalogueBtn").addEventListener("click", function() {
     window.open("https://drive.google.com/file/d/1DBaTebbTqqjZhnFlC67KEnmZ0jTaUbaT/view?usp=drivesdk", "_blank");
 });
+
+document.getElementById("whatsappBtn").addEventListener("click", function(){
+
+var phone = "+919384050357";
+
+var message = "Hello, I would like to know more about your products.";
+
+var whatsappApp = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+
+window.location.href = whatsappApp;
+
+});
