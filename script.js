386c0439e1b33f7f1002efbb1142be6280b0e4a1
samirmetalagencies <samirmetalagencies@gmail.com>
2026-03-07 18:21:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("catalogueBtn").addEventListener("click", function() {
+    window.open("https://drive.google.com/file/d/1DBaTebbTqqjZhnFlC67KEnmZ0jTaUbaT/view?usp=drivesdk", "_blank");
+});
